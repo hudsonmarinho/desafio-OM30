bafed7da46ba7c33e4f0ccecc75088dd50d89e2a
Hudson Marinho <hudsonmarinhodev@gmail.com>
2023-05-03 22:08:54 -0300

    feat(tailwind): add and config Tailwind

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import App from "./App.vue";
 import store from "./store";
 import router from "./router";
 
+import "@/assets/scss/application.scss";
+
 const app = createApp(App);
 app.use(store);
 app.use(router);
+
 app.mount("#app");
