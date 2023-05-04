54b2fb00682c3d3ff3e1a77e303fabe4ac750b1e
Hudson Marinho <hudsonmarinhodev@gmail.com>
2023-05-04 18:18:51 -0300

    feat(image): image component created

---
+++ src/components/Image/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <img :src="imageSrc" :alt="imageAlt" :title="imageTitle" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Image",
+  props: {
+    src: {
+      type: String,
+      required: true
+    },
+    alt: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      default: ""
+    }
+  },
+  computed: {
+    imageSrc() {
+      return this.src || "/user-default.png";
+    },
+    imageAlt() {
+      return this.alt || "Imagem Alt";
+    },
+    imageTitle() {
+      return this.title;
+    }
+  }
+});
+</script>
