6fd76cd2c22e19b9b3158e91570a12e072319c16
Hudson Marinho <hudsonmarinhodev@gmail.com>
2023-05-09 03:22:31 -0300

    feat: new patient page created

--- src/views/Admin/Patients/new.vue
+++ src/views/Admin/Patients/new.vue
@@ -1,12 +1,36 @@
 <template>
-  <h1>Novo Paciênte</h1>
-  <router-link to="/admin">Voltar</router-link>
+  <Layout>
+    <div class="md:w-3/4 w-full mx-auto mb-12 flex justify-between">
+      <h1 class="md:text-2xl text-base flex items-center">Novo Paciente</h1>
+
+      <button
+        @click="goBack()"
+        class="md:h-10 h-8 md:px-4 px-3 md:text-base text-sm rounded bg-blue-600 text-neutral-50 uppercase"
+      >
+        Voltar
+      </button>
+    </div>
+
+    <patient-form submitText="Cadastrar" />
+  </Layout>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
+import Layout from "@/layouts/Dashboard/Layout.vue";
+import PatientForm from "@/components/PatientForm/index.vue";
+
 export default defineComponent({
-  name: "AdminPatientNew"
+  name: "AdminPatientNew",
+  components: {
+    Layout,
+    PatientForm
+  },
+  methods: {
+    goBack() {
+      this.$router.push("/admin/");
+    }
+  }
 });
 </script>
