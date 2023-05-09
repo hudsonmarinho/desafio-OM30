ad7fc11bae3a7069f68779276ca20d3288ed7053
Hudson Marinho <hudsonmarinhodev@gmail.com>
2023-05-09 03:06:26 -0300

    refactor(404): refactored the Error 404 view

--- src/views/Erros/404/index.vue
+++ src/views/Erros/404/index.vue
@@ -1,6 +1,18 @@
 <template>
-  <div>
-    <h1>Page Not Found!</h1>
+  <div class="flex flex-col items-center justify-center text-center h-screen">
+    <h1 class="text-4xl font-bold text-gray-900 mb-4">
+      404: Página não encontrada
+    </h1>
+
+    <p class="text-lg text-gray-700 mb-8">
+      A página que você está procurando não existe.
+    </p>
+
+    <router-link
+      to="/"
+      class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+      >Voltar para a página inicial</router-link
+    >
   </div>
 </template>
 
